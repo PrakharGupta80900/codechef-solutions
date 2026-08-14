a63360cfe8c8c131622a4aa91e270d809702c721
PrakharGupta80900 <prakhargupta448@gmail.com>
2026-08-14 17:49:41 +0530

    ✅ Solved: PALL01 - The Block Game Practice Coding Problem [Java]

---
+++ java/PALL01.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            
+            int original=n;
+            int reverse=0;
+            while(n>0){
+                int digit=n%10;
+                reverse=reverse*10+digit;
+                n/=10;
+            }
+            if(reverse==original){
+                System.out.println("wins");
+            }else{
+                System.out.println("loses");
+            }
+        }
+	}
+}
