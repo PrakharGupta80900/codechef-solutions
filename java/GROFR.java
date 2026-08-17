8bf9cb244bc75c2b93b09408a47b7a92dff7215b
Prakhar Gupta <prakhargupta448@gmail.com>
2026-08-17 21:58:41 +0530

    ✅ Solved: GROFR - Practice Coding Problem [Java]

---
+++ java/GROFR.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String s = sc.next();
+
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1' && (i == 0 || s.charAt(i - 1) == '0')) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
