05caae5da41bee760fedca24a56f5623a2fdedbe
Prakhar Gupta <prakhargupta448@gmail.com>
2026-08-21 23:25:47 +0530

    ✅ Solved: UWCOI20A - Find maximum in an Array Practice Problem in Arrays [Java]

---
+++ java/UWCOI20A.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            int max=Integer.MIN_VALUE;
+            for(int num:arr){
+                if(num>max){
+                    max=num;
+                }
+            }
+            System.out.println(max);
+        }
+	}
+}
