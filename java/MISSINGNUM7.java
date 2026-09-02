59a20d8b8c844600576edb63781335dcc02eefbd
Prakhar Gupta <prakhargupta448@gmail.com>
2026-09-02 21:09:32 +0530

    ✅ Solved: MISSINGNUM7 - Practice Coding Problem [Java]

---
+++ java/MISSINGNUM7.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        
+        int sum=10;
+        System.out.println(10-s);
+	}
+}
