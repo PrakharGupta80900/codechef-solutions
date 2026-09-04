6239d06c17f4cb2938d50f3a15c171ab653d7b71
Prakhar Gupta <prakhargupta448@gmail.com>
2026-09-04 19:22:25 +0530

    ✅ Solved: MAXCONSECU - Chef and Consecutive Ones Practice Problem in Arrays [Java]

---
+++ java/MAXCONSECU.java
@@ -0,0 +1,16 @@
+    public static int findMaxConsecutiveOnes(int[] nums) {
+       //write code here...
+       int max=0;
+       int count=0;
+       for(int i=0;i<nums.length;i++){
+           if(nums[i]==1){
+               count++;
+               if(count>max){
+                   max=count;
+               }
+           }else{
+                count=0;   
+           }
+       }
+       return max;
+    }
