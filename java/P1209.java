98890f203ebc19572a376373c4c470ab962c04c9
Prakhar Gupta <prakhargupta448@gmail.com>
2026-09-05 23:34:59 +0530

    ✅ Solved: P1209 - Bitcoin Market Practice Coding Problem [Java]

---
+++ java/P1209.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        if(r>4){
+            System.out.println("No");
+        }else{
+            System.out.println("Yes");
+        }
+	}
+}
