cb85f11eb8c34fa64b117d1a0336798cbab87946
Prakhar Gupta <prakhargupta448@gmail.com>
2026-09-11 22:08:23 +0530

    ✅ Solved: DISCOOKIE - Distribute Cookies Practice Coding Problem [Java]

--- java/DISCOOKIE.java
+++ java/DISCOOKIE.java
@@ -6,23 +6,18 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t-->0){
-            int n=sc.nextInt();
-            int m=sc.nextInt();
-            int count=0;
-            while(m%n!=0){
-                if(m%n>n/2){
-                    count++;
-                    m++;
-                }else{
-                    count--;
-                    m--;
-                }
-            }
-            System.out.println(count);
-        }
+		Scanner sc = new Scanner (System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt(),m=sc.nextInt();
+		    if(n>=m){
+		        System.out.println(n-m);
+		        continue;
+		    }
+		    int rem=m%n;
+		    int a=rem,b=n-rem;
+		    System.out.println(Math.min(a,b));
+		}
+
 	}
 }
