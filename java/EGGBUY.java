853724b6fd586cef096f01995d0fe41bd7f2840c
Prakhar Gupta <prakhargupta448@gmail.com>
2026-09-13 22:40:44 +0530

    ✅ Solved: EGGBUY - Buying Eggs Practice Coding Problem [Java]

---
+++ java/EGGBUY.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        
+        int f=sc.nextInt();
+        
+        int costa=x*12;
+        int costb=y*12+f;
+        
+        System.out.println(Math.min(costa,costb));
+	}
+}
