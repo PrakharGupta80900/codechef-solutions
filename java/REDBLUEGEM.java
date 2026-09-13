2a4502627606746e1817fba760403b94def741a7
Prakhar Gupta <prakhargupta448@gmail.com>
2026-09-13 22:46:04 +0530

    ✅ Solved: REDBLUEGEM - Red and Blue Gems Practice Coding Problem [Java]

---
+++ java/REDBLUEGEM.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int b=sc.nextInt();
+        int p=sc.nextInt();
+        int q=sc.nextInt();
+        
+        System.out.println(Math.max(r*p,b*q));
+	}
+}
