e2b1ce14b42caba55979a3b33c89c4ffe90830eb
Prakhar Gupta <prakhargupta448@gmail.com>
2026-09-20 23:30:47 +0530

    ✅ Solved: SELLCOIN - Selling Coins Practice Coding Problem [Java]

---
+++ java/SELLCOIN.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+         Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A + 2 * B);
+        
+	}
+}
