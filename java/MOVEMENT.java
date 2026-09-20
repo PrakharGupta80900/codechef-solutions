1a244b0d7f8fd4c21065e27829e5b0587d3b2ec8
Prakhar Gupta <prakhargupta448@gmail.com>
2026-09-20 23:25:03 +0530

    ✅ Solved: MOVEMENT - Move Grid Practice Coding Problem [Java]

---
+++ java/MOVEMENT.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+
+        int x = A - C;
+        int y = B - D;
+
+        System.out.println(x + " " + y);
+        
+	}
+}
