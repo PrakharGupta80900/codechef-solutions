53b0aae6a38838a468ac76e2ccb8e7366674f14c
Prakhar Gupta <prakhargupta448@gmail.com>
2026-09-26 23:12:16 +0530

    ✅ Solved: COMP4 - Competition of 4 Practice Coding Problem [Java]

---
+++ java/COMP4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int x=sc.nextInt();
+		// your code goes here
+        if(x==1){
+            System.out.println(8000);
+        }else if(x==2){
+            System.out.println(4000);
+        }else if(x==3){
+            System.out.println(2000);
+        }else{
+            System.out.println(1000);
+        }
+	}
+}
